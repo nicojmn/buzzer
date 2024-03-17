cf49c02bc013454c76c880b189e150ac55200318
nicojmn <nicolasjeanmenne@gmail.com>
2024-03-18 00:19:30 +0100

    [frontend] basic skeleton

---
+++ frontend/src/Footer.svelte
@@ -0,0 +1,34 @@
+<footer>
+  <img id="tux-footer" src="img/tux_llnux_title_banner.svg" alt="Louvain-li-Nux banner" width="10%">
+  <p>Made with love by Louvain-li-Nux</p>
+  <img id="akap-footer" src="img/logo_akap.png" alt="Akapella logo" width="5%">
+</footer>
+
+
+<style>
+  footer {
+    background-color: #f0f0f0;
+    padding: 1em;
+    text-align: center;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+
+  }
+  
+  #tux-footer {
+    float: right;
+    margin-right: 35%;
+  }
+
+  #akap-footer {
+    float: left;
+    margin-left: 35%;
+  }
+
+  p {
+    font-size: 75%;
+  }
+</style>

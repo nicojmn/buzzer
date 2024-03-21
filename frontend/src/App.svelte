e2627a0ffc12585aa56516d52a5d0caa4adc3e61
nicojmn <nicolasjeanmenne@gmail.com>
2024-03-21 19:07:07 +0100

    [frontend] path update

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Button } from '@sveltestrap/sveltestrap';
-  import Footer from './Footer.svelte';
-  import Header from './Header.svelte';
+  import Footer from './lib/Footer.svelte';
+  import Header from './lib/Header.svelte';
 </script>
 
 <svelte:head>

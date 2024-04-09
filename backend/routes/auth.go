8210038b04c423c7a3900fd4dc8666308f8939d8
nicojmn <nicolasjeanmenne@gmail.com>
2024-04-09 16:25:05 +0200

    [backend] auth routes completion

--- backend/routes/auth.go
+++ backend/routes/auth.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"buzzer/auth"
+
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -11,6 +12,7 @@ func SetupAuthRoutes(app *fiber.App, db *gorm.DB) {
 	app.Get("/login", func(c *fiber.Ctx) error {
 		return c.SendFile("login.html")
 	})
+
 	app.Post("/login", func(c *fiber.Ctx) error {
 		username := c.FormValue("username")
 		password := c.FormValue("password")
@@ -20,15 +22,13 @@ func SetupAuthRoutes(app *fiber.App, db *gorm.DB) {
 			return c.SendString("Authentification failed, please retry")
 		}
 
-		return c.JSON(fiber.Map{"token": token})
+		c.Cookie(&fiber.Cookie{
+			Name:  "jwt",
+			Value: token,
+			HTTPOnly: true,
+			Secure: true,
+		})
 
-	})
-	app.Get("/protected", func(c *fiber.Ctx) error {
-		token := c.Get("Authorization")
-		if token == "" {
-			return c.SendString(fiber.ErrUnauthorized.Message)
-		}
-
-		return c.SendStatus(500)
+		return c.Redirect("/admin")
 	})
 }

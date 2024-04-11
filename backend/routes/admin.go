11fd7b12d8866df40180104a710f21c6c8c099e3
nicojmn <nicolasjeanmenne@gmail.com>
2024-04-11 17:25:02 +0200

    [backend] update route

--- backend/routes/admin.go
+++ backend/routes/admin.go
@@ -1,13 +1,18 @@
 package routes
 
 import (
+	"buzzer/auth"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupAdminRoutes(app *fiber.App) {
 	app.Route("/admin", func (admin fiber.Router) {
-		admin.Get("/", func(c *fiber.Ctx) error {
-			return fiber.ErrUnauthorized;
+		admin.Get("/dashboard", func(c *fiber.Ctx) error {
+			if (auth.IsLogged(c)) {
+				return c.SendFile("admin/dashboard/index.html")
+			} else {
+				return c.Redirect("/login")
+			}
 		})
 
 	})

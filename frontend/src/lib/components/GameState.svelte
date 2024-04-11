9a9c79bb47150da694ef22fec4c07ad361a9d1d2
nicojmn <nicolasjeanmenne@gmail.com>
2024-04-11 17:26:00 +0200

    [frontend] Game state for dashboard page

---
+++ frontend/src/lib/components/GameState.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    // WIP 
+    // TODO : fetch variables from backend
+    let buzzer_number:number = Math.round(Math.random() * 10)
+    let state:string = "close"
+    let podium:string[] = ["athjrjytkuytkty", "byjjytkeutk" ,"cyjtdytkdtykuktj(e"]
+</script>
+
+<div class="flex flex-row">
+    <a class="card" href="/admin/buzzers">
+        <span class="text-center"> Buzzer connectés : {buzzer_number}/13</span>
+    </a>
+    
+    <a class="card" href="/admin/buzzers">
+        <span class="text-center">État des buzzers : {state === "open" ? "ouverts" : "déverouillés"}</span><br>
+        {#if state === "close"}
+            <button class="btn-blue mt-1">Déverrouiller</button>
+        {:else}
+            <button class="btn-blue mt-1">Verrouiller</button>
+        {/if}
+    </a>
+
+    <div class="card">
+        {#each {length : 3} as _,i}
+            <span class="overflow-hidden"> Team #{i + 1} : {podium[i]}</span><br>
+        {/each}
+    </div>
+</div>
+
+
---
+++ frontend/src/routes/admin/dashboard/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import GameState from "$lib/components/GameState.svelte" 
+</script>
+<div> 
+    <GameState />
+</div>
